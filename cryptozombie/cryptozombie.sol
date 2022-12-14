96237fd9351212856cb1168a0490ef2847b2b8bb
PatrickSchubert87 <dev.analize@workmail.com>
2022-12-14 01:22:40 +0100

    Add day 2 homework - use external files

--- cryptozombie/cryptozombie.sol
+++ cryptozombie/cryptozombie.sol
@@ -17,15 +17,10 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    mapping (uint => address) public zombieToOwner;
-    mapping (address => uint) ownerZombieCount;
-
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
         uint id = zombies.length - 1;
         // and fire it here
-        zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
