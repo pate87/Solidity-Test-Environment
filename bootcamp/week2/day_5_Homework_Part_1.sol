7754f2cd2b7160a770a3adcbfbcca29744f12f4a
PatrickSchubert87 <dev.analize@workmail.com>
2022-12-14 02:02:06 +0100

    Add day 5 - Homework interface

---
+++ bootcamp/week2/day_5_Homework_Part_1.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+// Sign my contract by using the "signMe" function. 
+
+// Here is the address on Polygon POS: 0x3cdbff65dac67cdb6e5c4f05c4db8fe05c20e4d8
+
+// You have to give it who is signing as the first argument (an address).
+
+// You have to give it the signature (the message) as the second argument (a string).
+
+// It returns a string. 
+
+// Hint: Since you don't have the code, use an interface
+
+pragma solidity ^0.8.13;
+
+interface Itoken {
+    function signMe(address signer,string memory signature) external returns(string memory);
+}
+
+contract signContract {
+
+    function sign() public returns(string memory) {
+        Itoken signToken = Itoken(0x3CdBff65DaC67cDb6E5c4F05c4DB8FE05C20e4D8);
+        return signToken.signMe(msg.sender, "Hello thanks for this course");
+    }
+}
