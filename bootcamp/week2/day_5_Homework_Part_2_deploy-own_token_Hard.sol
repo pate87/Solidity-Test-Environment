b9d8d37ad83ab1e46932f0976ac6232f5ef43b87
PatrickSchubert87 <dev.analize@workmail.com>
2022-12-15 17:46:16 +0100

    Day 5 - Create own token with all necessary functions (Hard coded)

---
+++ bootcamp/week2/day_5_Homework_Part_2_deploy-own_token_Hard.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract OwnToken {
+
+    address private owner;
+    uint public totalSuply = 0;
+    string public name = "Patrick Token";
+    string public symbol = "PST";
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You're not the owner");
+        _;
+    }
+
+    mapping(address => uint) balances;
+
+    function getBalance(address _address) public view returns(uint) {
+        return balances[_address];
+    }
+
+    function mint(uint amaunt) public onlyOwner {
+        balances[msg.sender] += amaunt;
+        totalSuply += amaunt;
+    }
+
+    function transfer(address to, uint amaunt) public {
+        require(balances[msg.sender] >= amaunt, "You've not enough tokens");
+        require(to != address(0), "Can't send to 0 address");
+        balances[to] += amaunt;
+        balances[msg.sender] -= amaunt;
+    }
+}
