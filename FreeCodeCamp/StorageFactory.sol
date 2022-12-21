d01d5a3bbc74f82ce37b19ec2fd8b1a4b28c0f00
PatrickSchubert87 <dev.analyze@workmail.com>
2022-12-21 18:45:29 +0100

    Interacting with other contracts

--- FreeCodeCamp/StorageFactory.sol
+++ FreeCodeCamp/StorageFactory.sol
@@ -15,4 +15,17 @@ contract StorageFactory {
         // Add new created simpleStorage to the simpleStorageArray 
         simpleStorageArray.push(simpleStorage);
     }
+
+    function sfStore(uint256 _index, uint256 _number) public {
+        // SimpleStorage simpleStorage = simpleStorageArray[_index];
+        simpleStorageArray[_index].addPerson(_number, "This is my favorite store number");
+        // simpleStorage.addPerson(_number, "This is my favorite store number");
+    }
+
+    
+    function getStorageFactory(uint256 _index) public view returns(uint256) {
+        // SimpleStorage simpleStorage = simpleStorageArray[_index];
+        // return simpleStorage.retrieve();
+        return simpleStorageArray[_index].retrieve();
+    }
 }
