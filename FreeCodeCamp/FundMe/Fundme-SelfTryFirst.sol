2abb32757ff203fae962f3261df9e5feeb3c4f4f
PatrickSchubert87 <dev.analyze@workmail.com>
2022-12-21 21:17:18 +0100

    Fund Me - Self Try first

---
+++ FreeCodeCamp/FundMe/Fundme-SelfTryFirst.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.7;
+
+/*
+// Get funds from user
+// withdraw funds
+// set minimum funding value
+*/
+
+contract FundMe {
+
+    uint minimumFundsValue;
+    address[] public funderArray;
+    address owner;
+    uint public totalFunds;
+
+    constructor(uint _minimumFundsValue) {
+        owner = msg.sender;
+        // set minimum funding value
+        minimumFundsValue = _minimumFundsValue;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You're not the owner");
+        _;
+    }
+
+    mapping(address => uint) public fundsAmountOfAddress;
+
+    // Get funds from user
+    function fund(address payable funder, uint amount) public onlyOwner {
+        funderArray.push(funder);
+        fundsAmountOfAddress[funder] += amount;
+        totalFunds += amount;
+    }
+
+    // withdraw funds
+    function transferFunds(address to, uint amount) public onlyOwner {
+        require(totalFunds >= minimumFundsValue, "The minimum funds arent achived yet");
+        require(amount <= fundsAmountOfAddress[to], "You've not enough funds");
+        fundsAmountOfAddress[to] -= amount;
+        totalFunds -= amount;
+    }
+
+
+    // function setMinimumFundingValue(uint amount) public {
+    //     minimumFundsValue = _minimumFundsValue;
+    // }
+}
+
+
+// 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+// 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
