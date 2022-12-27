78d128045c6e644115837999d09c43cdc0ef541a
PatrickSchubert87 <dev.analyze@workmail.com>
2022-12-27 17:48:22 +0100

    Fund Me - Sending tokens through a function & reverts

--- FreeCodeCamp/FundMe/FundMe.sol
+++ FreeCodeCamp/FundMe/FundMe.sol
@@ -9,16 +9,52 @@ pragma solidity ^0.8.7;
 
 contract FundMe {
 
+    uint256 public minimumUsd = 50;
+
+    // uint256 minimumFundsValue;
+    // address[] public funderArray;
+    // address owner;
+    // uint public totalFunds;
+
+    // constructor(uint _minimumFundsValue) {
+    //     owner = msg.sender;
+    //     // set minimum funding value
+    //     minimumFundsValue = _minimumFundsValue;
+    // }
+
+    // modifier onlyOwner {
+    //     require(msg.sender == owner, "You're not the owner");
+    //     _;
+    // }
+
+    mapping(address => uint) public fundsAmountOfAddress;
+
     // Get funds from user
     // To make a function payable, marking the function as payable is necessary 
+    // function fund(address payable funder, uint amount) public payable onlyOwner {
     function fund() public payable {
         // Want to be able to set a minimum fund amount
         // 1. How to send tokens to this contract? 
-        require(msg.value > 1e18, "Didn't send enough!");
+        require(msg.value >= minimumUsd, "Didn't send enough!");
         // What is reverting? - Undo any action before, and send remaining gas back
 
+        // funderArray.push(funder);
+        // fundsAmountOfAddress[funder] += amount;
+        // totalFunds += amount;
     }
 
+    // withdraw funds
+    // function withdraw(address to, uint amount) public onlyOwner {
+    //     require(totalFunds >= minimumFundsValue, "The minimum funds arent achived yet");
+    //     require(amount <= fundsAmountOfAddress[to], "You've not enough funds");
+    //     fundsAmountOfAddress[to] -= amount;
+    //     totalFunds -= amount;
+    // }
+
+
+    // function setMinimumFundingValue(uint amount) public {
+    //     minimumFundsValue = _minimumFundsValue;
+    // }
 }
 
 
