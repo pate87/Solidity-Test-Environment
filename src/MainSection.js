50ac4f2c325980b5e07bc178a91d9df0656d74b0
dev <dev@admins-MBP.fritz.box>
2023-01-05 05:30:43 +0100

    Initial commit - create react test files to read and wirte from blockchain

--- src/MainSection.js
+++ src/MainSection.js
@@ -1,22 +1,81 @@
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+// Create new contractABI file and import ABI Remix data to read data from the contract 
+import ABI from "./contractABI.json";
 
 function MainSection() {
 
-    // Variables to print on the site 
+    // Create state variables to print on the site 
     const [currentAccount, setCurrentAccount] = useState(null);
     const [chainId, setChainId] = useState(null);
     const [chainName, setChainName] = useState(null);
     const [balance, setBalance] = useState(null);
     const [blockNumber, setBlockNumber] = useState(null);
 
-    // Creat arrow function that is a variable 
+    const [subscribed, setSubscribed] = useState(null);
+    const [age, setAge] = useState(null);
+    const [name, setName] = useState(null);
+
+    const [doubleAge, setDoubleAge] = useState(null);
+    const [addExclamation, setAddExclamation] = useState(null);
+
+    const [input, setInput] = useState(null);
+
+    const change = async () => {
+        // console.log("Test");
+        // Setting up the contract
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract("0x5312ffB4811D4f467f232aE7B97c8BD8044B7B2d", ABI, signer);
+
+        // const changeAge = await contract.setAge(30);
+        const changeName = await contract.setName(input);
+        // const changeSubscription = await contract.setSubscribtion();
+        const receipt = await changeName.wait();
+        console.log(receipt);
+
+    }
+
+    // Creat arrow function that is a variable and get the information of contract   
+    const getData = async () => {
+        // console.log("Hey");
+        // Creates a new Object and autofills it
+        // Get the information of wallet
+        // Setting up the contract with next 3 lines of code  
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        // Create instance to write to the contract 
+        const signer = provider.getSigner();
+        // Create instance of contract 1) Address 2) import ABI 3) just created signer variable
+        const contract = new ethers.Contract("0x5312ffB4811D4f467f232aE7B97c8BD8044B7B2d", ABI, signer);
+
+        // 1) set a variable 2) await = wait for response 3) call the contract from variable contract 4) call the public state variable from contract
+        const areTheySubscribed = await contract.subscribed();
+        // console.log(areTheySubscribed.toString());
+        // Need to be a toString() to show the output on the page as string otherwise it doesn't show correct 
+        setSubscribed(areTheySubscribed.toString());
+
+        const getAge = await contract.age();
+        setAge(getAge.toString());
+
+        const getName = await contract.name();
+        setName(getName.toString());
+
+        const contractDoubleAge = await contract.doubleAge();
+        setDoubleAge(contractDoubleAge.toString());
+
+        const contractAddExclamation = await contract.addExclamation();
+        setAddExclamation(contractAddExclamation.toString());
+
+    }
+
+    // Creat arrow function that is a variable and get the information of wallet
     const getWalletAddress = async () => {
         // console.log("Is working");
         // Look for a wallet on the browser 
         if(window.ethereum && window.ethereum.isMetaMask) {
             // console.log("Browser has a wallet");
-            // Creates a new Object and autofills it 
+            // Creates a new Object and autofills it
+            // Get the information of wallet 
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             // Looks for accounts on the wallet 
             await provider.send("eth_requestAccounts");
@@ -68,12 +127,20 @@ function MainSection() {
     return (
         <div class="MainSection">
             <div class="Content">
-                {/* <button onClick={getWalletAddress}>Connect</button> */}
                 <p>{currentAccount}</p>
                 <p>Chain id: {chainId}</p>
                 <p>Chain Name: {chainName}</p>
                 <p>Eth: {balance}</p>
                 <p>Block#: {blockNumber}</p>
+                <button onClick={getData}>GET DATA</button>
+                <p>subscribed: {subscribed}</p>
+                <p>Age: {age}</p>
+                <p>Double Age: {doubleAge}</p>
+                <p>Name: {name}</p>
+                <p>Name!: {addExclamation}</p>
+                <input value={input} onInput={e => setInput(e.target.value)} />
+                <p> {input} </p>
+                <button onClick={change}>Change Data</button>
             </div>
         </div>
     );
