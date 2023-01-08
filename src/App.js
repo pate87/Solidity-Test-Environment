fc2f863195684cb25daa6019e37dbbde511ec83d
Patrick <patrick@me.com>
2023-01-08 19:02:30 +0100

    Install Bootstrap for React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Header from './Header';
+import State from './State';
 // import MainSection from './MainSection';
 import Todo from './Todo';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
         <Header />
       </header>
       <main>
-  
+        <State />
         <Todo />
       </main>
     </div>
