ebdaec4d0913b1a912a4e2017cc426c7dd7e393b
PatrickSchubert87 <dev.analyze@workmail.com>
2023-02-01 06:14:33 +0100

    Day 1 - Setting up hack of auction contract

--- bootcamp/week9/day_1_Auction_Hack.sol
+++ bootcamp/week9/day_1_Auction_Hack.sol
@@ -15,4 +15,11 @@ contract auctionhack {
     function bid() public payable {
         auctioncontract.bid{value: msg.value}();
     }
+
+    // Possible way to receive tokens from a smart contract and in a smart contract 
+    // A fallback() can also be used to in case by calling a function which isn't available  
+    fallback() external payable {}
+
+    // Possible way to receive tokens from a smart contract and in a smart contract 
+    receive() external payable {}
 }
