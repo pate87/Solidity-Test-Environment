b0cadc9194fb043cadfc3c3c44c305cb1a0897a4
PatrickSchubert87 <dev.analyze@workmail.com>
2023-02-03 19:04:46 +0100

    Day 1 - Setting up auction contract and First Hack - No possibility to bid anymore after contract hack has bidden  - hack contract can receive tokens after update

--- bootcamp/week9/day_1_Auction.sol
+++ bootcamp/week9/day_1_Auction.sol
@@ -24,10 +24,10 @@ contract auction {
         require(running);
         require(msg.value > highestBid, "You're not the highest bidder");
 
-        // check wether (person a bids 1 ETH) < (Person b bids 2 ETH) and person b bedomes the new highestBidder
-        // If (current highestBidder) < (new highestBidder ) prviewes highestBidder gets his bid (tockens) back 
+        // If (current highestBidder) < (new highestBidder ) prviewes highestBidder gets his tockens back 
         payable(highestBidder).transfer(highestBid);
         // Smart contracts, by default, cannot accept ether 
+        
         highestBidder = msg.sender;
         highestBid = msg.value;
     }
--- bootcamp/week9/day_1_Auction_Hack.sol
+++ bootcamp/week9/day_1_Auction_Hack.sol
@@ -18,8 +18,12 @@ contract auctionhack {
 
     // Possible way to receive tokens from a smart contract and in a smart contract 
     // A fallback() can also be used to in case by calling a function which isn't available  
-    fallback() external payable {}
+    fallback() external payable {
+        // Remove this function for the exploit to work
+    }
 
     // Possible way to receive tokens from a smart contract and in a smart contract 
-    receive() external payable {}
+    receive() external payable {
+        // Remove this function for the exploit to work
+    }
 }
